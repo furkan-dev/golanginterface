208a8cc0ad18587a5cb4dc4610fdc385e0869b05
Furkan Akal <30433306+furkan-dev@users.noreply.github.com>
2017-08-31 21:40:55 +0300

    Add files via upload

---
+++ main.go
@@ -0,0 +1,62 @@
+//
+//
+// coder : furkan-dev
+//
+// licensed by gnu-general-license
+//
+//
+
+package main
+
+import "fmt"
+
+type contactInterface interface {
+
+	save()
+	update()
+	delete()
+
+}
+
+type contact struct {
+
+	personName string
+	personNumber string
+
+}
+
+func imple(k contactInterface) {
+
+	k.save()
+	k.update()
+	k.delete()
+
+}
+
+func (r contact) delete() {
+
+	fmt.Println(r.personName , "delete")
+
+}
+
+func (r contact) save() {
+
+	fmt.Println(r.personName , "save")
+
+}
+
+func (r contact) update() {
+
+	fmt.Println(r.personName , "update")
+
+}
+
+func main() {
+
+	personObject := contact{personName: "furkan", personNumber: "123"}
+
+	fmt.Println(personObject)
+
+	imple(personObject)
+
+}
